d5d9795e5ae8c5b9576d67836ed28d75475c5a37
jaeunP <okn2432@gmail.com>
2024-01-20 20:07:35 +0900

    init

---
+++ src/main/java/com/deliveryplatform/DeliveryPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.deliveryplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryPlatformApplication.class, args);
+	}
+
+}

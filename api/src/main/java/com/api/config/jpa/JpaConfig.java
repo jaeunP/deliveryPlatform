dabb78e0f46981e789afc9bc0b942c856fe00ecc
jaeunP <okn2432@gmail.com>
2024-01-23 00:04:21 +0900

    Config 설정 -JPA, ObjectMapper, Swagger

---
+++ api/src/main/java/com/api/config/jpa/JpaConfig.java
@@ -0,0 +1,11 @@
+package com.api.config.jpa;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EntityScan(basePackages = "com.db")
+@EnableJpaRepositories(basePackages = "com.db")
+public class JpaConfig {
+}
